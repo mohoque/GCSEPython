26489f8e2744ff628fd2f4d62e5431a07d565e4d
Mo Hoque <48127425+mohoque@users.noreply.github.com>
2023-06-13 23:38:40 +0100

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import openai
+
+# Initial system message
+initial_message = "You are a helpful assistant that teaches Python to a Year 10 student preparing for GCSE in a conversational manner. Always provide step-by-step explanations. Include example Python code in your responses when it would help to illustrate the concept."
+
+st.title('Python Learning Assistant')
+
+# Add a warning at the top of the application
+st.warning('**Disclaimer:** This app helps you learn Python and sometimes provides code examples. We encourage you to practice these examples in your own Python environment. However, do not run code that you do not understand, due to potential unforeseen results.')
+
+def main():
+    
+    # Input for OpenAI API Key
+    st.markdown("<div style='background-color: #ffa500; padding:10px; margin-bottom:10px;'>Input your OpenAI API Key:</div>", unsafe_allow_html=True)
+    openai.api_key = st.text_input("", type="password")
+
+    if 'num_questions' not in st.session_state:
+        st.session_state['num_questions'] = 0
+
+    if 'chat_history' not in st.session_state:
+        st.session_state['chat_history'] = []
+
+    user_input = st.text_input("Ask a Python question")
+
+    if st.button('Send'):
+        st.session_state['num_questions'] += 1
+
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=f"{initial_message}\nStudent asks a Python question: {user_input}\nHow would you teach them?",
+            max_tokens=500,
+        )
+        assistant_message = response.choices[0].text.strip()
+
+        st.session_state['chat_history'].insert(0, f'Assistant: {assistant_message}')
+        st.session_state['chat_history'].insert(0, f'**You: {user_input.replace("You: ", "")}**')
+        
+        for message in st.session_state['chat_history']:
+            st.markdown(message, unsafe_allow_html=True)
+        st.write(f'You have asked {st.session_state["num_questions"]} questions.')
+
+if __name__ == "__main__":
+    main()
