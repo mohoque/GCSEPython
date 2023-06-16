cecc3abb2d9daebb464cf363b4dda7852ffe9ef8
Mo Hoque <48127425+mohoque@users.noreply.github.com>
2023-06-16 16:49:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import openai
+from streamlit_chat import message
 
 # Initial system message
 initial_message = "You are a helpful assistant that teaches Python to a Year 10 student preparing for GCSE in a conversational manner. Always provide step-by-step explanations. Include example Python code in your responses when it would help to illustrate the concept."
@@ -26,18 +27,23 @@ def main():
     if st.button('Send'):
         st.session_state['num_questions'] += 1
 
+        # Generate a response to the user's question
         response = openai.Completion.create(
             model="text-davinci-003",
             prompt=f"{initial_message}\nStudent asks a Python question: {user_input}\nHow would you teach them?",
             max_tokens=500,
         )
+
         assistant_message = response.choices[0].text.strip()
 
-        st.session_state['chat_history'].insert(0, f'Assistant: {assistant_message}')
-        st.session_state['chat_history'].insert(0, f'**You: {user_input.replace("You: ", "")}**')
-        
-        for message in st.session_state['chat_history']:
-            st.markdown(message, unsafe_allow_html=True)
+        # Add the user's message and the assistant's response to the chat history
+        st.session_state['chat_history'].insert(0, [user_input, assistant_message])
+
+        # Display the chat history
+        for chat in st.session_state['chat_history']:
+            message(f'You: {chat[0]}', is_user=True)
+            message(f'Assistant: {chat[1]}')
+
         st.write(f'You have asked {st.session_state["num_questions"]} questions.')
 
 if __name__ == "__main__":
